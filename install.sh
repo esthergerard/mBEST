bb6392915dfb88307917ecc529cee8981d3a0281
esthergerard <esther-gerard@orange.fr>
2023-10-04 06:00:40 -0700

    Better Install script AiO

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+pip install -r requirements.txt
+sudo apt install libxcb-*
+conda install -c conda-forge libstdcxx-ng
 python3 setup.py build_ext --inplace
 python3 mBEST/utils/numba_utils.py
